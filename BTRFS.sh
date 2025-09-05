66ac2a155c50f5e15f997ea649541781ef8bc59c
Etienne Rodrigue <3812081+EtienneRod@users.noreply.github.com>
2025-09-05 08:46:42 -0400

    Update BTRFS.sh

--- BTRFS.sh
+++ BTRFS.sh
@@ -2,18 +2,18 @@
 
 # Set temporary PATH
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin;
-export PATH="$PATH:test";
+export PATH='$PATH:test';
 
 # Dedup /mnt/Share/Medias/Pictures
-echo "Deduping /mnt/Share/Medias/Pictures";
-duperemove -r -d /mnt/Share/Medias/Pictures;
+bash -c "echo 'Deduping /mnt/Share/Medias/Pictures'";
+bash -c "duperemove -r -d /mnt/Share/Medias/Pictures";
 
 # Scrub Data and Metadata to make sure data is not corrupted
-echo "Scrubing Data and Metadata";
-btrfs scrub start -Bq /mnt/Share;
+bash -c "echo 'Scrubing Data and Metadata'";
+bash -c "btrfs scrub start -Bq /mnt/Share";
 
 # Modify Backup.log owner
-echo "Modifying log owner";
-chown Etienne:Etienne /home/Etienne/HK/*.log;
+bash -c "echo 'Modifying log owner'";
+bash -c "chown Etienne:Etienne /home/Etienne/HK/*.log";
 
 exit 0;
